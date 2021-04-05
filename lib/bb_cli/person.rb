f9e3d562a1ad97b20c5fe2846a5709aa03b2de6b
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-04-05 16:03:12 -0700

    slef.all and save methods added to the Person Class

--- lib/bb_cli/person.rb
+++ lib/bb_cli/person.rb
@@ -1,10 +1,21 @@
 class Person
     attr_accessor :name, nickname, :occupation, :status 
 
+    @@all = []
+
     def initialize(name, nickname, occupation, status)
         @name = name
         @nickname = nickname
         @occupation = occupation
         @status = status
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 end
