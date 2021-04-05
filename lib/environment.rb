d243a9a33da5b40f548640113fccee57c6bb1f3c
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-04-05 15:31:07 -0700

    Environment and Run Progress

---
+++ lib/environment.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+#require_relative "cli/version"
+require "pry"
+
+require "bundler"          
+Bundler.require         #these two lines of code allow us to require everthing in our gem file
+
+require_all "./lib/bb_cli"
+
+
+
