5b8cdeb83ec2eefc55d6cef787319fe0d9d8f123
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-04-15 11:52:03 -0700

    commit tree 1e9a2b0aa144a491cf509884123fa80980a02a8a

--- lib/bb_cli/api.rb
+++ lib/bb_cli/api.rb
@@ -3,8 +3,9 @@ class API
     def self.get_data
         data_response = RestClient.get("https://www.breakingbadapi.com/api/characters") 
         person_array = JSON.parse(data_response)       
-        person_array.each do |peeps|
-            Person.new(peeps)
+        person_array.each do |person_ob|
+            Person.new(person_ob)
+            binding.pry
         end
     end                      
     
